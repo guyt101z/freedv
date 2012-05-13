e01154e0e8e07f84b9e4fea38a94bd913e7dcf09
David Witten <wittend@wwrinc.com>
2012-05-13 15:30:39 +0000

    General Program Options Dialog
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@429 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_options.cpp
@@ -0,0 +1,29 @@
+//==========================================================================
+// Name:            dlg_options.cpp
+//
+// Purpose:         Creates simple wxWidgets dialog GUI general program
+//                  devices/options.
+//
+// Date:            May 06 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#include "dlg_options.h"
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+// Class OptionsDlg
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+OptionsDlg::OptionsDlg( wxWindow* parent ) : DlgOptions( parent )
+{
+}
+
+void OptionsDlg::OnClose( wxCloseEvent& event )
+{
+// TODO: Implement OnClose
+}
+
+void OptionsDlg::OnInitDialog( wxInitDialogEvent& event )
+{
+// TODO: Implement OnInitDialog
+}
