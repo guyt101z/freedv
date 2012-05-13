ac28394492b5cf8b37cb03dd884e0af9a71e83d9
David Witten <wittend@wwrinc.com>
2012-05-13 15:25:24 +0000

    help>About Dialog.
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@423 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_about.cpp
@@ -0,0 +1,21 @@
+//==========================================================================
+// Name:            dlg_about.h
+//
+// Purpose:         Creates simple wxWidgets dialog GUI for the
+//                  help/about page.
+//
+// Date:            May 05 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#include "dlg_about.h"
+
+AboutDlg::AboutDlg( wxWindow* parent ) : DlgAbout( parent )
+{
+}
+
+void AboutDlg::OnInitDialog( wxInitDialogEvent& event )
+{
+// TODO: Implement OnInitDialog
+}
