bca6e53b2cbb380a9f576bf9c6a5f72f284cab7c
David Witten <wittend@wwrinc.com>
2012-05-13 15:27:22 +0000

    Audio Dialog
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@425 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_audio.cpp
@@ -0,0 +1,32 @@
+//==========================================================================
+// Name:            dlg_audio.cpp
+//
+// Purpose:         Creates simple wxWidgets dialog GUI for audio
+//                  devices/options.
+//
+// Date:            May 05 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#include "dlg_audio.h"
+
+AudioDlg::AudioDlg( wxWindow* parent ) : DlgAudio( parent )
+{
+
+}
+
+void AudioDlg::OnCancel( wxCommandEvent& event )
+{
+    this->EndModal(wxID_OK);
+}
+
+void AudioDlg::OnOK( wxCommandEvent& event )
+{
+    this->EndModal(wxID_OK);
+}
+
+void AudioDlg::OnClose( wxCloseEvent& event )
+{
+    this->EndModal(wxID_OK);
+}
