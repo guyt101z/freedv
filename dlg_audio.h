7e4427f1ddb13cb1cb937cd09a7e984227d9ffb4
David Witten <wittend@wwrinc.com>
2012-05-13 15:27:58 +0000

    Audio Dialog Header
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@426 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_audio.h
@@ -0,0 +1,30 @@
+//==========================================================================
+// Name:              dlg_audio.h
+//
+// Purpose:           Subclasses dialog GUI for audio devices/options.
+//                  (DlgAudio from topFrame.h)
+//
+// Date:            May 05 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#ifndef __AUDIO_DIALOG__
+#define __AUDIO_DIALOG__
+
+#include "topFrame.h"
+
+class AudioDlg : public DlgAudio
+{
+    protected:
+        // Handlers for DlgAudio events.
+        void OnCancel( wxCommandEvent& event );
+        void OnOK( wxCommandEvent& event );
+        void OnClose( wxCloseEvent& event );
+    public:
+        /** Constructor */
+        AudioDlg( wxWindow* parent );
+
+};
+
+#endif // __AUDIO_DIALOG__
