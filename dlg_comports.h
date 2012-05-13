7422327ae7384e7994ff2eb6f7eadd1eaa403c26
David Witten <wittend@wwrinc.com>
2012-05-13 15:29:55 +0000

    Comm Ports Dialog Header
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@428 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_comports.h
@@ -0,0 +1,28 @@
+//==========================================================================
+// Name:            dlg_options.h
+// Purpose:         Subclasses dialog GUI for general program options.
+//                  (DlgOptions from topFrame.h)
+// Created:         May. 11, 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#ifndef __COMPORTS_DIALOG__
+#define __COMPORTS_DIALOG__
+
+#include "topFrame.h"
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+// Class ComPortsDlg
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+class ComPortsDlg : public DlgComPorts
+{
+    protected:
+        // Handlers for events.
+        void OnListItemSelected( wxListEvent& event );
+    public:
+        /** Constructor */
+        ComPortsDlg( wxWindow* parent );
+};
+
+#endif // __COMPORTS_DIALOG__
