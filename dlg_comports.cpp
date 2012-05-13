7077a715a4f6419094f8fc4fe23629b1c9232e30
David Witten <wittend@wwrinc.com>
2012-05-13 15:29:07 +0000

    Comm Ports Dialog
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@427 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ dlg_comports.cpp
@@ -0,0 +1,22 @@
+//==========================================================================
+// Name:            dlg_comports.cpp
+// Purpose:         Creates simple wxWidgets dialog GUI to select
+//                  real/virtual Comm ports.
+// Date:            May 11 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#include "dlg_comports.h"
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+// Class ComPortsDlg
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+ComPortsDlg::ComPortsDlg( wxWindow* parent ) : DlgComPorts( parent )
+{
+}
+
+void ComPortsDlg::OnListItemSelected( wxListEvent& event )
+{
+    // TODO: Implement OnListItemSelected
+}
