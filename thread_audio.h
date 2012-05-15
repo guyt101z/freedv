6e94e96735db22f0ddc052674cdd0b102de21b76
David Witten <wittend@wwrinc.com>
2012-05-15 13:44:43 +0000

    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@466 01035d8c-6547-0410-b346-abe4f91aad63

---
+++ thread_audio.h
@@ -0,0 +1,26 @@
+//==========================================================================
+// Name:            thread_audio.h
+//
+// Purpose:         Declares background thread classes to handle audio IO.
+// Created:         May 11, 2012
+// Initial author:  David Witten
+// License:         BSD License (other licenses may apply to other
+//                  components of this project)
+//==========================================================================
+#include <wx/thread.h>
+#include "fdmdv2_main.h"
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+// Class AudioThread
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=
+class AudioThread: public wxThread
+{
+    public:
+        AudioThread(MainFrame *handler) : wxThread(wxTHREAD_DETACHED) { m_pHandler = handler }
+        ~AudioThread();
+
+    protected:
+        virtual ExitCode Entry();
+        MainFrame *m_pHandler;
+};
+
