0475457d612a673bc4f043a8c3240e833eb82776
David Witten <wittend@wwrinc.com>
2012-08-17 20:57:31 +0000

    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@627 01035d8c-6547-0410-b346-abe4f91aad63

--- fdmdv2.mk
+++ fdmdv2.mk
@@ -7,14 +7,14 @@ ProjectName            :=fdmdv2
 ConfigurationName      :=Release
 IntermediateDirectory  :=./Release
 OutDir                 := $(IntermediateDirectory)
-WorkspacePath          := "C:\Users\wittend\Projects\Radio\fdmdv2"
-ProjectPath            := "C:\Users\wittend\Projects\Radio\fdmdv2"
+WorkspacePath          := "C:\bin\Projects\Radio\fdmdv2"
+ProjectPath            := "C:\bin\Projects\Radio\fdmdv2"
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=wittend
-Date                   :=8/13/2012
-CodeLitePath           :="C:\Program Files\CodeLite"
+User                   :=OFA-Staff
+Date                   :=8/17/2012
+CodeLitePath           :="C:\Program Files (x86)\CodeLite"
 LinkerName             :=g++
 ArchiveTool            :=ar rcus
 SharedObjectLinkerName :=g++ -shared -fPIC
@@ -35,13 +35,13 @@ Preprocessors          :=$(PreprocessorSwitch)__WX__
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
 PreprocessOnlySwitch   :=-E 
-ObjectsFileList        :="C:\Users\wittend\Projects\Radio\fdmdv2\fdmdv2.txt"
+ObjectsFileList        :="C:\bin\Projects\Radio\fdmdv2\fdmdv2.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 CmpOptions             := -O2 -Wall $(shell wx-config --cxxflags --unicode=yes --debug=no) $(Preprocessors)
 C_CmpOptions           := -O2 -Wall $(shell wx-config --cxxflags --unicode=yes --debug=no) $(Preprocessors)
 LinkOptions            :=  -mwindows -s $(shell wx-config --debug=no --libs --unicode=yes)
-IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)/bin/MinGW/msys/1.0/local/include $(IncludeSwitch)../codec2-dev/src 
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)/bin/MinGW/msys/1.0/local/include $(IncludeSwitch)../codec2-dev/src $(IncludeSwitch)../../Audio/portaudio/include $(IncludeSwitch)../../Audio/portaudio/bindings/cpp/include $(IncludeSwitch)./libsndfile/include 
 IncludePCH             := 
 RcIncludePath          := 
 Libs                   := $(LibrarySwitch)sndfile-1 $(LibrarySwitch)codec2 $(LibrarySwitch)portaudio $(LibrarySwitch)portaudiocpp 
@@ -51,7 +51,7 @@ LibPath                := $(LibraryPathSwitch). $(LibraryPathSwitch)/bin/MinGW/m
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Program Files\CodeLite
+CodeLiteDir:=C:\Program Files (x86)\CodeLite
 WXWIN:=C:\bin\wxWidgets-2.9.4
 PATH:=$(WXWIN)\lib\gcc_dll;$(PATH)
 WXCFG:=gcc_dll\mswu
@@ -80,68 +80,68 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/topFrame$(ObjectSuffix): topFrame.cpp $(IntermediateDirectory)/topFrame$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/topFrame.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/topFrame$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/topFrame.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/topFrame$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/topFrame$(DependSuffix): topFrame.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/topFrame$(ObjectSuffix) -MF$(IntermediateDirectory)/topFrame$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/topFrame.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/topFrame$(ObjectSuffix) -MF$(IntermediateDirectory)/topFrame$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/topFrame.cpp"
 
 $(IntermediateDirectory)/topFrame$(PreprocessSuffix): topFrame.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/topFrame$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/topFrame.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/topFrame$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/topFrame.cpp"
 
 $(IntermediateDirectory)/dlg_about$(ObjectSuffix): dlg_about.cpp $(IntermediateDirectory)/dlg_about$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_about.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_about$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/dlg_about.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_about$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/dlg_about$(DependSuffix): dlg_about.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_about$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_about$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_about.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_about$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_about$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/dlg_about.cpp"
 
 $(IntermediateDirectory)/dlg_about$(PreprocessSuffix): dlg_about.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_about$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_about.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_about$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/dlg_about.cpp"
 
 $(IntermediateDirectory)/dlg_audio$(ObjectSuffix): dlg_audio.cpp $(IntermediateDirectory)/dlg_audio$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_audio.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_audio$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/dlg_audio.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_audio$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/dlg_audio$(DependSuffix): dlg_audio.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_audio$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_audio$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_audio.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_audio$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_audio$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/dlg_audio.cpp"
 
 $(IntermediateDirectory)/dlg_audio$(PreprocessSuffix): dlg_audio.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_audio$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_audio.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_audio$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/dlg_audio.cpp"
 
 $(IntermediateDirectory)/dlg_options$(ObjectSuffix): dlg_options.cpp $(IntermediateDirectory)/dlg_options$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_options.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_options$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/dlg_options.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_options$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/dlg_options$(DependSuffix): dlg_options.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_options$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_options$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_options.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_options$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_options$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/dlg_options.cpp"
 
 $(IntermediateDirectory)/dlg_options$(PreprocessSuffix): dlg_options.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_options$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_options.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_options$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/dlg_options.cpp"
 
 $(IntermediateDirectory)/dlg_comports$(ObjectSuffix): dlg_comports.cpp $(IntermediateDirectory)/dlg_comports$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_comports.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_comports$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/dlg_comports.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/dlg_comports$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/dlg_comports$(DependSuffix): dlg_comports.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_comports$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_comports$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_comports.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/dlg_comports$(ObjectSuffix) -MF$(IntermediateDirectory)/dlg_comports$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/dlg_comports.cpp"
 
 $(IntermediateDirectory)/dlg_comports$(PreprocessSuffix): dlg_comports.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_comports$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/dlg_comports.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/dlg_comports$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/dlg_comports.cpp"
 
 $(IntermediateDirectory)/fdmdv2_plot$(ObjectSuffix): fdmdv2_plot.cpp $(IntermediateDirectory)/fdmdv2_plot$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_plot.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/fdmdv2_plot$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/fdmdv2_plot.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/fdmdv2_plot$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/fdmdv2_plot$(DependSuffix): fdmdv2_plot.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/fdmdv2_plot$(ObjectSuffix) -MF$(IntermediateDirectory)/fdmdv2_plot$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_plot.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/fdmdv2_plot$(ObjectSuffix) -MF$(IntermediateDirectory)/fdmdv2_plot$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/fdmdv2_plot.cpp"
 
 $(IntermediateDirectory)/fdmdv2_plot$(PreprocessSuffix): fdmdv2_plot.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/fdmdv2_plot$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_plot.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/fdmdv2_plot$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/fdmdv2_plot.cpp"
 
 $(IntermediateDirectory)/fdmdv2_main$(ObjectSuffix): fdmdv2_main.cpp $(IntermediateDirectory)/fdmdv2_main$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_main.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/fdmdv2_main$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/fdmdv2_main.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/fdmdv2_main$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/fdmdv2_main$(DependSuffix): fdmdv2_main.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/fdmdv2_main$(ObjectSuffix) -MF$(IntermediateDirectory)/fdmdv2_main$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_main.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/fdmdv2_main$(ObjectSuffix) -MF$(IntermediateDirectory)/fdmdv2_main$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/fdmdv2_main.cpp"
 
 $(IntermediateDirectory)/fdmdv2_main$(PreprocessSuffix): fdmdv2_main.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/fdmdv2_main$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/fdmdv2_main.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/fdmdv2_main$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/fdmdv2_main.cpp"
 
 $(IntermediateDirectory)/paclass$(ObjectSuffix): paclass.cpp $(IntermediateDirectory)/paclass$(DependSuffix)
-	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/Users/wittend/Projects/Radio/fdmdv2/paclass.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/paclass$(ObjectSuffix) $(IncludePath)
+	$(CompilerName) $(IncludePCH) $(SourceSwitch) "C:/bin/Projects/Radio/fdmdv2/paclass.cpp" $(CmpOptions) $(ObjectSwitch)$(IntermediateDirectory)/paclass$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/paclass$(DependSuffix): paclass.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/paclass$(ObjectSuffix) -MF$(IntermediateDirectory)/paclass$(DependSuffix) -MM "C:/Users/wittend/Projects/Radio/fdmdv2/paclass.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/paclass$(ObjectSuffix) -MF$(IntermediateDirectory)/paclass$(DependSuffix) -MM "C:/bin/Projects/Radio/fdmdv2/paclass.cpp"
 
 $(IntermediateDirectory)/paclass$(PreprocessSuffix): paclass.cpp
-	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/paclass$(PreprocessSuffix) "C:/Users/wittend/Projects/Radio/fdmdv2/paclass.cpp"
+	@$(CompilerName) $(CmpOptions) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/paclass$(PreprocessSuffix) "C:/bin/Projects/Radio/fdmdv2/paclass.cpp"
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
@@ -175,6 +175,6 @@ clean:
 	$(RM) $(IntermediateDirectory)/paclass$(PreprocessSuffix)
 	$(RM) $(OutputFile)
 	$(RM) $(OutputFile).exe
-	$(RM) "C:\Users\wittend\Projects\Radio\fdmdv2\.build-release\fdmdv2"
+	$(RM) "C:\bin\Projects\Radio\fdmdv2\.build-release\fdmdv2"
 
 
--- fdmdv2_wsp.mk
+++ fdmdv2_wsp.mk
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo ----------Building project:[ pa_enum - Release ]----------
-	@cd "pa_enum" && "mingw32-make.exe"  -j 2 -f "pa_enum.mk"
+	@echo ----------Building project:[ fdmdv2 - Release ]----------
+	@"mingw32-make.exe"  -j 2 -f "fdmdv2.mk"
 clean:
-	@echo ----------Cleaning project:[ pa_enum - Release ]----------
-	@cd "pa_enum" && "mingw32-make.exe"  -j 2 -f "pa_enum.mk" clean
+	@echo ----------Cleaning project:[ fdmdv2 - Release ]----------
+	@"mingw32-make.exe"  -j 2 -f "fdmdv2.mk" clean
