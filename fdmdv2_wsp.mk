0c226b09ddf14136679b2e268c58a766941720be
David Witten <wittend@wwrinc.com>
2012-08-20 19:43:10 +0000

    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@628 01035d8c-6547-0410-b346-abe4f91aad63

--- fdmdv2_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ fdmdv2 - Release ]----------
-	@"mingw32-make.exe"  -j 2 -f "fdmdv2.mk"
-clean:
-	@echo ----------Cleaning project:[ fdmdv2 - Release ]----------
-	@"mingw32-make.exe"  -j 2 -f "fdmdv2.mk" clean
