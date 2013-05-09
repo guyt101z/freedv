e7a742fa21256520a202f5ec2bd76c2a9176dc6e
Richard Shaw <hobbes1069@gmail.com>
2013-05-09 20:44:20 +0000

    Fix internal building of Codec2. Fix finding libraries under MinGW.
    git-svn-id: https://freetel.svn.sourceforge.net/svnroot/freetel/fdmdv2@1245 01035d8c-6547-0410-b346-abe4f91aad63

--- cmake/BuildCodec2.cmake
+++ cmake/BuildCodec2.cmake
@@ -4,8 +4,13 @@ ExternalProject_Add(codec2
    CMAKE_ARGS -DBUILD_SHARED_LIBS=FALSE 
    INSTALL_COMMAND ""
 )
-find_library(CODEC2_LIBRARIES codec2
-    PATHS ${CMAKE_BINARY_DIR}/codec2-prefix/src/codec2-build/src)
+if(WIN32)
+    set(CODEC2_LIBRARIES
+        ${CMAKE_BINARY_DIR}/codec2-prefix/src/codec2-build/src/codec2.lib)
+else(WIN32)
+    set(CODEC2_LIBRARIES
+        ${CMAKE_BINARY_DIR}/codec2-prefix/src/codec2-build/src/libcodec2.a)
+endif(WIN32)
 include_directories(${CMAKE_BINARY_DIR}/codec2-prefix/src/codec2/src)
 list(APPEND FREEDV_LINK_LIBS ${CODEC2_LIBRARIES})
 list(APPEND FREEDV_STATIC_DEPS codec2)
